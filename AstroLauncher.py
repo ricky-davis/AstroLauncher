994531981b22d98ecb0ed4bdff0e0c8276de2bd5
Ricky Davis <rdavis@Davis.lan>
2020-06-09 10:10:37 -0500

    autopep8 formatting

--- AstroLauncher.py
+++ AstroLauncher.py
@@ -1,7 +1,7 @@
 
 import AstroAPI
 
-import argparse 
+import argparse
 import atexit
 import ctypes
 import json
@@ -24,23 +24,26 @@ from pprint import pprint, pformat
 
 '''
 
+
 class AstroLauncher():
     """ Starts a new instance of the Server Launcher"""
-    
+
     def __init__(self, astropath):
         self.astropath = astropath
-        
-        formatter = logging.Formatter('%(asctime)s - %(levelname)-6s %(message)s',datefmt="%Y-%m-%d %H:%M:%S")
+
+        formatter = logging.Formatter(
+            '%(asctime)s - %(levelname)-6s %(message)s', datefmt="%Y-%m-%d %H:%M:%S")
         rootLogger = logging.getLogger()
         rootLogger.setLevel(logging.INFO)
 
         console = logging.StreamHandler()
         console.setFormatter(formatter)
 
-        logsPath = os.path.join(astropath,'logs\\')
+        logsPath = os.path.join(astropath, 'logs\\')
         if not os.path.exists(logsPath):
             os.makedirs(logsPath)
-        fileLogHandler = TimedRotatingFileHandler(os.path.join(astropath,'logs',"server.log"),  'midnight', 1)
+        fileLogHandler = TimedRotatingFileHandler(os.path.join(
+            astropath, 'logs', "server.log"),  'midnight', 1)
         fileLogHandler.setFormatter(formatter)
 
         rootLogger.addHandler(console)
@@ -50,24 +53,24 @@ class AstroLauncher():
 
         self.settings = AstroAPI.get_current_settings(astropath)
         self.headers = AstroAPI.base_headers
-        self.activePlayers=[]
+        self.activePlayers = []
         self.ipPortCombo = f'{self.settings["publicip"]}:{self.settings["port"]}'
-        self.headers['X-Authorization'] = AstroAPI.generate_XAUTH(self.settings['serverguid'])
+        self.headers['X-Authorization'] = AstroAPI.generate_XAUTH(
+            self.settings['serverguid'])
 
         atexit.register(self.kill_server, "Launcher shutting down")
         self.start_server()
 
-    def logPrint(self,message):
+    def logPrint(self, message):
         logging.info(pformat(message))
-
 
     def kill_server(self, reason):
         self.logPrint(f"Kill Server: {reason}")
         self.deregister_all_server()
-        ## Kill all child processes
+        # Kill all child processes
         try:
             for child in psutil.Process(self.process.pid).children():
-                #print(child)
+                # print(child)
                 child.kill()
         except:
             pass
@@ -77,24 +80,28 @@ class AstroLauncher():
         self.logPrint("Starting Server process...")
         time.sleep(3)
         startTime = time.time()
-        cmd = [os.path.join(self.astropath,"AstroServer.exe"), '-log']
+        cmd = [os.path.join(self.astropath, "AstroServer.exe"), '-log']
         self.process = subprocess.Popen(cmd)
 
-        if os.path.samefile(sys.executable,sys.argv[0]):
-            daemonCMD = [sys.executable, '--daemon', '-l', str(os.getpid()), '-c', str(self.process.pid)]
+        if os.path.samefile(sys.executable, sys.argv[0]):
+            daemonCMD = [sys.executable, '--daemon', '-l',
+                         str(os.getpid()), '-c', str(self.process.pid)]
         else:
-            daemonCMD = [sys.executable, sys.argv[0], '--daemon', '-l', str(os.getpid()), '-c', str(self.process.pid)]
+            daemonCMD = [sys.executable, sys.argv[0], '--daemon',
+                         '-l', str(os.getpid()), '-c', str(self.process.pid)]
         #print(' '.join(daemonCMD))
 
-        self.watchDogProcess = subprocess.Popen(daemonCMD, shell=False, creationflags=subprocess.DETACHED_PROCESS | subprocess.CREATE_NEW_PROCESS_GROUP)
+        self.watchDogProcess = subprocess.Popen(
+            daemonCMD, shell=False, creationflags=subprocess.DETACHED_PROCESS | subprocess.CREATE_NEW_PROCESS_GROUP)
 
         # Wait for server to finish registering...
         registered = False
         apiRateLimit = 2
         while registered == False:
             try:
-                serverData = (AstroAPI.get_server(self.ipPortCombo,self.headers))
-                #pprint(serverData)
+                serverData = (AstroAPI.get_server(
+                    self.ipPortCombo, self.headers))
+                # pprint(serverData)
                 serverData = serverData['data']['Games']
                 lobbyIDs = [x['LobbyID'] for x in serverData]
                 if len(set(lobbyIDs) - set(oldLobbyIDs)) == 0:
@@ -105,16 +112,19 @@ class AstroLauncher():
                     self.LobbyID = serverData[0]['LobbyID']
 
                 if self.process.poll() != None:
-                    self.logPrint("Server was forcefully closed before registration. Exiting....")
+                    self.logPrint(
+                        "Server was forcefully closed before registration. Exiting....")
                     return False
             except Exception as e:
-                self.logPrint("Failed to check server. Probably hit rate limit. Backing off and trying again...")
+                self.logPrint(
+                    "Failed to check server. Probably hit rate limit. Backing off and trying again...")
                 apiRateLimit += 1
                 time.sleep(apiRateLimit)
 
         doneTime = time.time()
         elapsed = doneTime - startTime
-        self.logPrint(f"Server ready with ID {self.LobbyID}. Took {round(elapsed,2)} seconds to register.")
+        self.logPrint(
+            f"Server ready with ID {self.LobbyID}. Took {round(elapsed,2)} seconds to register.")
 
         self.server_loop()
 
@@ -126,33 +136,36 @@ class AstroLauncher():
             curPlayers = self.DSListPlayers()
             if curPlayers is not None:
                 if len(curPlayers) > len(self.activePlayers):
-                    playerDif = list(set(curPlayers) - set(self.activePlayers))[0]
+                    playerDif = list(set(curPlayers) -
+                                     set(self.activePlayers))[0]
                     self.activePlayers = curPlayers
                     self.logPrint(f"Player joining: {playerDif}")
                 elif len(curPlayers) < len(self.activePlayers):
-                    playerDif = list(set(self.activePlayers) - set(curPlayers))[0]
+                    playerDif = list(
+                        set(self.activePlayers) - set(curPlayers))[0]
                     self.activePlayers = curPlayers
                     self.logPrint(f"Player left: {playerDif}")
 
             time.sleep(2)
-    
-    
+
     def DSListPlayers(self):
         with AstroLauncher.session_scope(self.settings['consoleport']) as s:
             s.sendall(b"DSListPlayers\n")
             rawdata = AstroLauncher.recvall(s)
             parsedData = AstroLauncher.parseData(rawdata)
-            #pprint(parsedData)
+            # pprint(parsedData)
             try:
                 return [x['playerName'] for x in parsedData['playerInfo'] if x['inGame'] == True]
             except:
                 return None
 
     def deregister_all_server(self):
-        servers_registered = (AstroAPI.get_server(self.ipPortCombo,self.headers))['data']['Games']
+        servers_registered = (AstroAPI.get_server(
+            self.ipPortCombo, self.headers))['data']['Games']
         if (len(servers_registered)) > 0:
-            self.logPrint(f"Attemping to deregister all ({len(servers_registered)}) servers as {self.ipPortCombo}")
-            #pprint(servers_registered)
+            self.logPrint(
+                f"Attemping to deregister all ({len(servers_registered)}) servers as {self.ipPortCombo}")
+            # pprint(servers_registered)
             for reg_srvr in servers_registered:
                 self.logPrint(f"Deregistering {reg_srvr['LobbyID']}..")
                 AstroAPI.deregister_server(reg_srvr['LobbyID'], self.headers)
@@ -160,13 +173,13 @@ class AstroLauncher():
             time.sleep(1)
             return [x['LobbyID'] for x in servers_registered]
         return []
-    
+
     @staticmethod
     @contextmanager
-    def session_scope(consolePort : int):
+    def session_scope(consolePort: int):
         try:
             s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            #s.settimeout(5)
+            # s.settimeout(5)
             s.connect(("127.0.0.1", int(consolePort)))
             yield s
         except:
@@ -174,10 +187,10 @@ class AstroLauncher():
         finally:
             s.close()
 
-    @staticmethod    
+    @staticmethod
     def recvall(sock):
         try:
-            BUFF_SIZE = 4096 # 4 KiB
+            BUFF_SIZE = 4096  # 4 KiB
             data = b''
             while True:
                 part = sock.recv(BUFF_SIZE)
@@ -206,15 +219,20 @@ def watchDog(laucherPID, consolePID):
             os.kill(child.pid, signal.CTRL_C_EVENT)
     except Exception as e:
         print(e)
-        
+
+
 if __name__ == "__main__":
     try:
         parser = argparse.ArgumentParser()
-        parser.add_argument("-p", "--path", help = "Set the server folder path", type=str.lower)
-        parser.add_argument("-d", "--daemon", dest="daemon", help = "Set the launcher to run as a Daemon", action='store_true')
-        
-        parser.add_argument("-c", "--consolepid", help = "Set the consolePID for the Daemon", type=str.lower)
-        parser.add_argument("-l", "--launcherpid", help = "Set the launcherPID for the Daemon", type=str.lower)
+        parser.add_argument(
+            "-p", "--path", help="Set the server folder path", type=str.lower)
+        parser.add_argument("-d", "--daemon", dest="daemon",
+                            help="Set the launcher to run as a Daemon", action='store_true')
+
+        parser.add_argument(
+            "-c", "--consolepid", help="Set the consolePID for the Daemon", type=str.lower)
+        parser.add_argument(
+            "-l", "--launcherpid", help="Set the launcherPID for the Daemon", type=str.lower)
         args = parser.parse_args()
         if args.daemon:
             if args.consolepid and args.launcherpid:
