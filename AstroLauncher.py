5b736c2c83713bc681f2f786d42948eabb3ff35f
Ricky Davis <ricky.c.davis.9@gmail.com>
2020-06-17 23:42:58 -0500

    Version update

--- AstroLauncher.py
+++ AstroLauncher.py
@@ -125,7 +125,7 @@ class AstroLauncher():
         self.refresh_launcher_config()
         if disable_auto_update is not None:
             self.launcherConfig.DisableAutoUpdate = disable_auto_update
-        self.version = "v1.4.1"
+        self.version = "v1.4.2"
         self.latestURL = "https://github.com/ricky-davis/AstroLauncher/releases/latest"
         self.isExecutable = os.path.samefile(sys.executable, sys.argv[0])
         self.headers = AstroAPI.base_headers
