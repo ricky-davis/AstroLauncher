bc769f035a7d7d914de20e0c20df2314b384f43b
Ricky Davis <ricky.c.davis.9@gmail.com>
2020-10-11 20:52:10 -0500

    Version update v1.7.6

--- AstroLauncher.py
+++ AstroLauncher.py
@@ -211,7 +211,7 @@ class AstroLauncher():
             astroPath=self.astroPath, logRetention=int(self.launcherConfig.LogRetentionDays))
         if disable_auto_update is not None:
             self.launcherConfig.DisableAutoUpdate = disable_auto_update
-        self.version = "v1.7.5"
+        self.version = "v1.7.6"
         colsize = os.get_terminal_size().columns
         if colsize >= 77:
             vText = "Version " + self.version[1:]
